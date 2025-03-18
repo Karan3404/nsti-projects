305d851cdbb06b265dcb7fbc697b1594b77ea151
Karan3404 <karansahu.nsti@gmail.com>
2025-03-18 12:06:24 +0530

    Add files via upload

---
+++ karan/html_and_php_project/contact_us.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+<div id="box">
+    <h3 id="contact_heading">CONTACT US</h3>
+    <pre><b>NSTI PUNCTUE WALA</b>
+    
+    address:near nsti kolkata dasnagar,howrah
+    mobile:64478512544
+    email:puncture_expert@gmail.com
+    
+</pre>
+</div>
+
+
+<form action="">
+    <div class="container">
+        
+        </div>
+        
+        
+    </form>
+    
+    
+    <?php include 'footer.php'; ?>
---
+++ karan/operators/6string/string.php
@@ -0,0 +1,14 @@
+<?php
+//php has two operator that are specially designed for strings.
+
+//concatenation
+$a = "hello";
+$b = "world";
+echo $a.$b;
+
+//concatenation assignment
+echo "<br>";
+$a.=$b;
+echo $a;
+
+?>
