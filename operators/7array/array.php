a8e36ddc8226ac7cdb923c427c024a9583c9301a
Karan Sahu <karansahu.nsti@gmail.com>
2025-03-18 12:03:50 +0530

    all operators done

--- operators/7array/array.php
+++ operators/7array/array.php
@@ -3,16 +3,25 @@
 
 //union (combine two array and remove duplicates)
 
-$a =  array(1,2);
-$b = [1,3];
-
-echo $a[0];
-echo $b[0];
-
+$a =  array(1,2,3);
+$b =  array(4,2,4,5);
+$c = $a+$b;
+print_r($c);
 echo "<br>";
 
-$c=[1,2]+[1,3];
-print_r($c);
+// equality (check if two array have same key values)
+$d = array(1,2);
+$e = array(1,2);
+$f = $d==$e;
+print_r($f);
+echo "<br>";
+
+// identity 
+$g = array(1,2);
+$h = array(1,2);
+$i = $d===$e;
+print_r($i);
+
 
 
 
